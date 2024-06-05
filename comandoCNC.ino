37700e9f49440c425c03af44d9f6b6447f0eae58
Eduardo Eugênio <146144048+duduardo45@users.noreply.github.com>
2024-06-05 15:00:09 -0300

    Feito restart e andar dois eixos
    Consertei os bugs do restart, e aceita agora comando de andar pela Serial.

--- comandoCNC.ino
+++ comandoCNC.ino
@@ -1,16 +1,15 @@
 #include <AccelStepper.h>
+#include <GFButton.h>
 
-int botaoX = 24; // digitalRead(botaoX) 0 caso apertado; 1 caso aberto
-int botaoY = 22;
+GFButton botaoX(24); // digitalRead(botaoX) 0 caso apertado; 1 caso aberto
+GFButton botaoY(22);
 
 bool reiniciar = false;
 
 bool reiniciarX = false;
-int confiaX = 0;
 unsigned long ultResetX = 0;
 
 bool reiniciarY = false;
-int confiaY = 0;
 unsigned long ultResetY = 0;
 
 int pinSTEPx = 2;
@@ -26,113 +25,122 @@ AccelStepper stepperX(AccelStepper::DRIVER, pinSTEPx, pinDIRx);
 AccelStepper stepperY1(AccelStepper::DRIVER, pinSTEPy1, pinDIRy1);
 AccelStepper stepperY2(AccelStepper::DRIVER, pinSTEPy2, pinDIRy2);
 
+void stepY(long d) {
+  stepperY1.move(d);
+  stepperY2.move(d);
+}
 
+void paraX() {
+  reiniciarX = false;
+  Serial.println("parou X");
+  stepperX.stop();
+  return;
+}
+
+void paraY() {
+  reiniciarY = false;
+  Serial.println("parou Y");
+  stepperY1.stop();
+  stepperY2.stop();
+  return;
+}
+
+void andaPara( long x, long y) {
+  stepperX.move(x);
+  stepY(y);
+  return;
+}
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
 
-  stepperX.setMaxSpeed(200);
-  stepperX.setAcceleration(200);
+  stepperX.setMaxSpeed(250);
+  stepperX.setAcceleration(2000);
 
-  stepperY1.setMaxSpeed(200);
-  stepperY1.setAcceleration(200);
+  stepperY1.setMaxSpeed(250);
+  stepperY1.setAcceleration(2000);
   
-  stepperY2.setMaxSpeed(200);
-  stepperY2.setAcceleration(200);
+  stepperY2.setMaxSpeed(250);
+  stepperY2.setAcceleration(2000);
+
+  botaoX.setPressHandler(paraX);
+  botaoY.setPressHandler(paraY);
 }
 
 void loop() {
 
-  // if (stepperX.distanceToGo() == 0)
-  //   {
+  botaoX.process();
+  botaoY.process();
 
-  //   }
-    stepperX.run();
-    stepperY1.run();
-    stepperY2.run();
+  // if (stepperX.distanceToGo() != 0) {}
+  // if (stepperY1.distanceToGo() != 0) {}
 
+  stepperX.run();
+  stepperY1.run();
+  stepperY2.run();
+
+  // Logica de reiniciar a CNC para a posição inicial
   if (reiniciar) {
-    if (digitalRead(botaoX) == 0 && reiniciarX) {
-      confiaX++;
-      if (confiaX > 2){
-        reiniciarX = false;
-        stepperX.move(50);
-      }
-    }
-    if (digitalRead(botaoY) == 0 && reiniciarY) {
-      confiaY++;
-      if (confiaY > 3){
-        reiniciarY = false;
-        stepperY1.move(50);
-        stepperY2.move(50);
-      }
-    }
-    if (digitalRead(botaoX) == 1) {
-      confiaX = 0;
-      if (millis()-ultResetX > 200){
+
+    if (reiniciarX) {
+      if (millis()-ultResetX > 50){
         stepperX.move(-50);
         ultResetX = millis();
       }
     }
-    if (digitalRead(botaoY) == 1) {
-      confiaY = 0;
-      if (millis()-ultResetY > 200){
+
+    if (reiniciarY) {
+      if (millis()-ultResetY > 50){
         stepperY1.move(-50);
         stepperY2.move(-50);
         ultResetY = millis();
       }
     }
-    // ESTÁ BUGADO AQUI; COM ESTA PARTE ACABA CEDO DEMAIS
-    if (reiniciarX == false && reiniciarY == false) {
+  
+    if ((!reiniciarX) && (!reiniciarY)) {
+      Serial.println("parou");
       reiniciar = false;
+      // stepperX.setCurrentPosition(0);
+      // stepperY1.setCurrentPosition(0);
+      // stepperY2.setCurrentPosition(0);
     }
+
   }
 
   if (Serial.available() > 0) {
     String texto = Serial.readStringUntil('\n');
     texto.trim();
-    if (texto == "anda x"){
-      Serial.println(texto);
-      stepperX.move(500);
-      texto = "";
-    }
-    if (texto == "desanda x"){
-      Serial.println(texto);
-      stepperX.move(-100);
-      texto = "";
-    }
-    if (texto == "anda y"){
-      Serial.println(texto);
-      stepperY1.move(100);
-      stepperY2.move(100);
-      texto = "";
-    }
-    if (texto == "desanda y"){
-      Serial.println(texto);
-      stepperY1.move(-100);
-      stepperY2.move(-100);
-      texto = "";
-    }
-    if (texto == "anda"){
-      Serial.println(texto);
-      stepperX.move(200);
-      stepperY1.move(200);
-      stepperY2.move(200);
-      texto = "";
-    }
-    if (texto == "le") {
-      Serial.print("B X: ");
-      Serial.println(digitalRead(botaoX));
-      Serial.print("B Y: ");
-      Serial.println(digitalRead(botaoY));
-    }
+
     if (texto == "reinicia") {
       Serial.println(texto);
       reiniciar = true;
       reiniciarX = true;
       reiniciarY = true;
+      texto = "";
+    }
+    if (texto.startsWith("anda")) { // enviar: anda XXXX YYYY (aceitando negativos)
+      Serial.println(texto);
+      texto = texto.substring(6);
+      long x;
+      long y;
+      if (texto[0] == '-'){
+        x = texto.substring(0,5).toInt();
+        texto = texto.substring(1);
+      }
+      else { 
+        x = texto.substring(0,4).toInt();
+      }
+      texto = texto.substring(5);
+      if (texto[0] == '-') {
+        y = texto.substring(0,5).toInt();
+        texto = texto.substring(1);
+      }
+      else {
+        y = texto.substring(0,4).toInt();
+      }
+      andaPara(x,y);
+      texto = "";
     }
   }
-  
 }
