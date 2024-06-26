b951f5bddc87a713f0c60dc12229106f5d0d3cab
LuizFelipe <luizfneves@proton.me>
2024-06-26 15:37:05 -0300

    stuff at the end

--- UI/detect_and_classify/integration.py
+++ UI/detect_and_classify/integration.py
@@ -3,31 +3,24 @@ import time
 from collections import defaultdict
 
 import cv2
-import detect_category
+import detect_category as detect_category
 import opencv_feat_match as feat_match
 import opencv_simple_poly as simple_poly
 import serial
 
-DONE_MESSAGE = "reiniciado"
+DONE_MESSAGE = "pega terminado"
 SERIAL_PORT = "/dev/ttyACM0"  # Replace with your serial port
 BAUD_RATE = 9600
+CAMERA_NUMBER = 0
 
 print("Connecting to Arduino...")
 # Initialize serial communication
 ser = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=1)
 time.sleep(2)  # Wait for the connection to initialize
 
-ser.write("anda 0100 0100\n".encode("utf-8"))
 
-time.sleep(5)
-
-ser.write("reinicia\n".encode("utf-8"))
-time.sleep(10)
-print("Sent to Arduino: reinicia")
-
-
-def capture_image_from_webcam(output_path):
-    cap = cv2.VideoCapture(2)
+def capture_image_from_webcam(output_path: str):
+    cap = cv2.VideoCapture(CAMERA_NUMBER)
     if not cap.isOpened():
         print("Error: Could not open webcam.")
         return None
@@ -46,17 +39,20 @@ def capture_image_from_webcam(output_path):
     return frame
 
 
-def send_centroids_to_arduino(position_x, position_y, centroid):
-    print(f"Sending to Arduino: {position_x} {position_y} {centroid}")
-    time.sleep(3)  # Small delay to ensure the command is processed
-    print("Arduino responded: Done")
-    # Initialize serial communication
-    ser = serial.Serial(serial_port, baud_rate, timeout=1)
-    time.sleep(2)  # Wait for the connection to initialize
+def convert_pixel_centroid_to_arduino(centroid):
+    image_x = 3840 - 3840 / 8 - 3840 / 5
+    image_y = 2160
+    arduino_x = 1680
+    arduino_y = 1800
+    arduino_centroid_x = ((centroid[0] - 3840 / 5) / image_x) * arduino_x
+    arduino_centroid_y = (centroid[1] / image_y) * arduino_y
+    return arduino_x - arduino_centroid_x, arduino_y - arduino_centroid_y
 
-    coord_str = (
-        f"pega {box_number} {str(centroid[0]).zfill(4)},{str(centroid[1]).zfill(4)}\n"
-    )
+
+def send_centroids_to_arduino(position_x, position_y, centroid):
+    arduino_x, arduino_y = convert_pixel_centroid_to_arduino(centroid)
+
+    coord_str = f"pega {1} {str(arduino_x).zfill(4)} {str(arduino_y).zfill(4)}\n"
     ser.write(coord_str.encode("utf-8"))
     print(f"Sent to Arduino: {coord_str.strip()}")
     time.sleep(0.1)  # Small delay to ensure the command is processed
@@ -66,14 +62,14 @@ def send_centroids_to_arduino(position_x, position_y, centroid):
         response = ser.readline().decode("utf-8").strip()
         print(f"Received from Arduino: {response}")
         if response == DONE_MESSAGE:
-            print("Expected message received. Closing serial connection.")
+            print("Expected message received. Stopping send_centroids_to_arduino.")
             break
 
 
 def test_walk(position_x, position_y):
-    coord_str = f"pega 1 {str(position_x).zfill(4)} {str(position_y).zfill(4)}\n"
+    coord_str = f"pega 1 {str(position_x).zfill(4)} {str(position_y).zfill(4)}"
     ser.write(coord_str.encode("utf-8"))
-    print(f"Sent to Arduino: {coord_str.strip()}")
+    print(f"Sent to Arduino: '{coord_str}'")
 
     while True:
         # Read a line from the serial port
@@ -137,9 +133,7 @@ def process_polygon_objects(poly_objects, captured_image_path):
 
 
 def take_photo_command_arduino(objects: list[dict]):
-    captured_image_path = os.path.join(
-        "detect_and_classify", "test_images", "dois_white_rects.jpg"
-    )
+    captured_image_path = os.path.join("webcam", "webcam_photo.jpg")
 
     image_objects = [obj for obj in objects if obj["mode"] == "image"]
     poly_objects = defaultdict(list)
@@ -148,14 +142,21 @@ def take_photo_command_arduino(objects: list[dict]):
         if obj["mode"] == "polygon":
             poly_objects[(obj["color"], obj["polygon"])].append(obj)
 
+    ser.write("anda 0100 0100\n".encode("utf-8"))
+    print("Sent to Arduino: anda 0100 0100")
+    time.sleep(5)
+
     while True:
-        # capture_image_from_webcam(captured_image_path)
+        ser.write("reinicia\n".encode("utf-8"))
+        print("Sent to Arduino: reinicia")
+        time.sleep(15)
+        capture_image_from_webcam(captured_image_path)
 
         if process_image_objects(image_objects, captured_image_path):
-            break  # Stop if centroids were found for image objects
+            continue  # Continue if centroids were found for image objects
 
         if process_polygon_objects(poly_objects, captured_image_path):
-            break  # Stop if centroids were found for polygon objects
+            continue  # Continue if centroids were found for polygon objects
 
         print("No centroids found, sorting ended.")
         break
@@ -165,12 +166,12 @@ def take_photo_command_arduino(objects: list[dict]):
 
 
 if __name__ == "__main__":
-    capture_image_from_webcam(os.path.join("webcam", "webcam_photo.jpg"))
+    # capture_image_from_webcam(os.path.join("webcam", "webcam_photo.jpg"))
     database_objects = [
         {
             "name": "Brazilian Real",
-            "position_x": 1,
-            "position_y": 1,
+            "position_x": 1000,
+            "position_y": 1500,
             "exemplar_image_path": os.path.join(
                 "exemplar_images", "50_Brazil_real_Second_Reverse.jpeg"
             ),
@@ -181,8 +182,8 @@ if __name__ == "__main__":
         },
         {
             "name": "Cartão de fidelidade do Tabu Club",
-            "position_x": 2,
-            "position_y": 2,
+            "position_x": 800,
+            "position_y": 1500,
             "mode": "polygon",
             "color": "white",
             "polygon": "rectangle",
@@ -190,13 +191,13 @@ if __name__ == "__main__":
         },
         {
             "name": "Cartão da HotZone",
-            "position_x": 3,
-            "position_y": 3,
+            "position_x": 1200,
+            "position_y": 1500,
             "mode": "polygon",
             "color": "white",
             "polygon": "rectangle",
             "descricao": "Cartão usado na HotZone para brincar nos brinquedos",
         },
     ]
-    # take_photo_command_arduino(database_objects)
-    test_walk(1000, 1000)
+    take_photo_command_arduino(database_objects)
+    # test_walk(1300, 1000)
